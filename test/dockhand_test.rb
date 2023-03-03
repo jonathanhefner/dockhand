166c054c1e07415ab872f1fc936b287d56cf2047
Jonathan Hefner <jonathan@hefner.pro>
2023-03-03 12:38:12 -0600

    Remove default test

--- test/dockhand_test.rb
+++ test/dockhand_test.rb
@@ -2,12 +2,8 @@
 
 require "test_helper"
 
-class TestDockhand < Minitest::Test
+class DockhandTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Dockhand::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
